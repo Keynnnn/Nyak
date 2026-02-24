65078cece68a9a15494be81e0b1830a07a2c8dea
Keynnnn <luz.calimlim112178@gmail.com>
2026-02-24 17:58:24 +0800

    5.cpp

---
+++ 5.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main () {
+    string name = "Lloyd";
+    string course = "Civil Engineering"; // BSCE
+    int year = 2025;
+
+    cout << "Name: " << name << endl ;
+    cout << "Course: " << course << endl ;
+    cout << "Year: " << year << endl ;
+    return 0;
+}
